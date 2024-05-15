a8c1a11cbc5df3fcf39b53ab27be62b2040c9410
eliezersamuel <eliezer_sss@hotmail.com>
2024-05-15 19:38:24 -0300

    feat: hook with subscribe in main.tsx, outside a component

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { AppProvider } from "./context/index.tsx";
+import {useBearStore} from './store';
+
+useBearStore.subscribe(state => console.log('new state:', state));
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <AppProvider>
