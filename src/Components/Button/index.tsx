ff664fa4a0bfd582cc05cc512e10c98197c41258
eliezersamuel <eliezer_sss@hotmail.com>
2024-05-15 19:31:59 -0300

    feat: another way to use zustand hook store

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,6 +1,7 @@
 import { useBearStore } from "../../store";
 
 export default function Button() {
-  const increase = useBearStore((state) => state.increase);
+  // const increase = useBearStore((state) => state.increase);
+  const { increase } = useBearStore();
   return <button onClick={() => increase()}>increase</button>;
 }
